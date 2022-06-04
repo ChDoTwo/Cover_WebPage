b8a9b3ef55c2d7639c753eecabf73599a5c45d9f
ChDoTwo <engnl322@gmail.com>
2022-06-04 17:42:52 +0900

    Update_Web_Rco

--- 02_RCO/02_js/main.js
+++ 02_RCO/02_js/main.js
@@ -5,3 +5,18 @@ var swiper_01 = new Swiper(".banner",{
       prevEl: ".banner .swiper-button-prev",
     },
 });
+
+
+$('.testimonials .slide_btn').on('click','span',function(){
+
+  var btn_i = $(this).index();
+
+  if (btn_i == 0){
+    $('.testimonials .container').animate({marginLeft: '0'},1000);
+  } else if (btn_i == 1) {
+    $('.testimonials .container').animate({marginLeft: '-100%'},1000);
+  }
+
+  $('.testimonials .slide_btn > span').removeClass('selected');
+  $(this).addClass('selected');
+});
